33e414898d4b9c85410f40644a5088bd5902d847
vsricharan16 <scvempat@uci.edu>
2022-02-26 02:45:56 -0800

    Adding few beans

--- src/main/java/com/example/easyrental/EasyRentalApplication.java
+++ src/main/java/com/example/easyrental/EasyRentalApplication.java
@@ -1,11 +1,12 @@
 package com.example.easyrental;
 
 import com.example.easyrental.dao.UserRepository;
-import com.example.easyrental.model.Users;
+import com.example.easyrental.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 
 @SpringBootApplication
 public class EasyRentalApplication implements CommandLineRunner {
@@ -19,16 +20,16 @@ public class EasyRentalApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        Users user=new Users();
-        user.setFirstName("Hardik");
-        user.setLastName("Ruparel");
-        user.setEmail("abc@gmail.com");
-        user.setCountry("USA");
-        user.setCity("Irvine");
-        user.setPassword("abc123");
-        user.setState("CA");
-        user.setAddressLine1("abc lane");
-        user.setZipcode(123456);
-        userRepository.save(user);
+//        User user=new User();
+//        user.setFirstName("Hardik");
+//        user.setLastName("Ruparel");
+//        user.setEmail("abc@gmail.com");
+//        user.setCountry("USA");
+//        user.setCity("Irvine");
+//        user.setPassword("abc123");
+//        user.setState("CA");
+//        user.setAddressLine1("abc lane");
+//        user.setZipcode(123456);
+        //userRepository.save(user);
     }
 }
--- src/main/java/com/example/easyrental/dao/UserRepository.java
+++ src/main/java/com/example/easyrental/dao/UserRepository.java
@@ -1,9 +1,9 @@
 package com.example.easyrental.dao;
 
-import com.example.easyrental.model.Users;
+import com.example.easyrental.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<Users, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
 }
